fd1c9c33b977a8ecff982b3a9ef4d33e96608374
huzaks <jeremylavalleegiroux@gmail.com>
2023-09-24 21:01:45 -0400

    final changes 24 sept

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,42 +5,82 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import logo from "@/app/risewhite.png";
 import { GiHockey, GiSoccerBall } from "react-icons/gi";
+import { IoHome } from "react-icons/io5";
+import { FiSettings } from "react-icons/fi";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 export default function Sidebar() {
   const pathname = usePathname();
   return (
     <>
-      <div className="p-4 min-h-[100vh] w-52 bg-[#00092C] flex flex-col gap-4">
+      <nav className="relative p-4 min-h-[100vh] w-64 bg-[#00061d] flex flex-col gap-4">
         <Link href={"/"} className="text-5xl font-bold">
-          <Image src={logo} alt="Logo Rise" />
+          <Image src={logo} alt="Logo Rise" className="" />
         </Link>
-        <div className="text-white text-sm">
-            Sports
-        <Link
-          href={"/hockey"}
-          className={`text-lg flex gap-2 items-center mt-1 ml-4 ${
-            pathname.includes("hockey")
-              ? "text-[#B20600]"
-              : "text-white hover:text-[#B20600] transition-all"
-          }`}
-        >
-          <GiHockey />
-          Hockey
-        </Link>
-        <Link
-          href={"/soccer"}
-          className={`text-lg flex gap-2 items-center mt-1 ml-4 ${
-            pathname.includes("soccer")
-              ? "text-[#B20600]"
-              : "text-white hover:text-[#B20600] transition-all"
-          }`}
-        >
-          <GiSoccerBall />
-          Soccer
-        </Link>
+        <div className="flex flex-col text-white text-sm gap-1">
+          Général
+          <Link
+            href={"/"}
+            className={`font-semibold rounded-sm px-2  text-lg flex gap-2 items-center mt-1  ${
+              pathname === "/"
+                ? "bg-slate-200/[.30]"
+                : "text-white hover:bg-slate-200/[.30] transition-all"
+            }`}
+          >
+            <IoHome />
+            Accueil
+          </Link>
         </div>
-        
-        {/* <Link href={'/soccer'} className={`underline text-xl ${pathname.includes("soccer") ? "text-blue-800" : ''}`}>Soccer</Link> */}
-      </div>
+        <div className="flex flex-col text-white text-sm gap-1">
+          Sports
+          <Link
+            href={"/hockey"}
+            className={`font-semibold rounded-sm px-2  text-lg flex gap-2 items-center mt-1  ${
+              pathname.includes("hockey")
+                ? "bg-slate-200/[.30]"
+                : "text-white hover:bg-slate-200/[.30] transition-all"
+            }`}
+          >
+            <GiHockey />
+            Hockey
+          </Link>
+          <Link
+            href={"/soccer"}
+            className={`font-semibold rounded-sm px-2 text-lg flex gap-2 items-center mt-1 ${
+              pathname.includes("soccer")
+                ? "bg-slate-200/[.30]"
+                : "text-white hover:bg-slate-200/[.30] transition-all"
+            }`}
+          >
+            <GiSoccerBall />
+            Soccer
+          </Link>
+        </div>
+
+        <div className="absolute bottom-4 flex flex-col text-white text-sm gap-6">
+          <Link
+            href={"/profile/settings"}
+            className={`font-semibold rounded-sm px-2  text-lg flex gap-2 items-center ${
+              pathname === "/profile/settings"
+                ? "bg-slate-200/[.30]"
+                : "text-white hover:bg-slate-200/[.30] transition-all"
+            }`}
+          >
+            <FiSettings />
+            Réglages
+          </Link>
+          <div className="flex gap-2">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div>
+            <p className="font-bold">Utilisateur</p>
+            <p>utilisateur@email.com</p>
+          </div>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
