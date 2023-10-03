c5209620c9c73389e011ebcbc8e98cc14a4cedb8
huzaks <jeremylavalleegiroux@gmail.com>
2023-10-03 19:00:10 -0400

    button retour ce que nous offrons

--- src/app/ce-que-nous-offrons/page.jsx
+++ src/app/ce-que-nous-offrons/page.jsx
@@ -1,9 +1,20 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
-
+import { FiArrowLeft } from "react-icons/fi";
 export default function page() {
   return (
-    <div className="flex flex-col mx-auto items-center justify-center gap-16 px-4 lg:flex-row h-[150vh] md:h-[100vh]">
+    <div className="flex flex-col mx-auto items-center justify-center">
+      <div className="pl-4 pt-4 md:pt-0 w-fit mt-28 mb-8 self-start">
+        <Link
+          href={"/ringuette"}
+          className="group flex gap-4 bg-transparent hover:bg-[#00061d] border-4 border-[#00061d] text-[#00061d] hover:text-white rounded-[50px] py-2 px-4 text-md font-bold items-center justify-center transition-all "
+        >
+          <FiArrowLeft className=" text-[#00061d] text-lg group-hover:text-white transition-all" />
+          Retour
+        </Link>
+      </div>
+      <div className=" gap-16 px-4 lg:flex-row h-[150vh] md:h-[100vh]">
+      
       <div className="grid grid-cols-1 md:grid-cols-3 gap-12 h-[100%] md:h-[60%]">
         <Card className="relative p-4">
           <CardHeader>
@@ -81,6 +92,7 @@ export default function page() {
           </CardContent>
         </Card>
       </div>
+    </div>
     </div>
   );
 }
